7759aedd67979832e4595f3ed26712304c7ea511
toolateralus <joshnjoshgaming@gmail.com>
2023-09-14 15:15:34 -0400

    more to hardware

--- VM/Hardware/IHardware.cs
+++ VM/Hardware/IHardware.cs
@@ -4,106 +4,130 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
-
-/// <summary>
-/// This enum controls the level of access a particular IHardware device emulator grants to the user.
-/// </summary>
-public enum UserPermissions
-{
-    /// <summary>
-    /// No access to the hardware resource
-    /// </summary>
-    None,          
-    /// <summary>
-    /// Read-only access
-    /// </summary>
-    Read,
-    /// <summary>
-    ///  Write-only access
-    /// </summary>
-    Write,         
-    /// <summary>
-    /// Read and write access
-    /// </summary>
-    ReadWrite,    
-    /// <summary>
-    /// Control or manage the hardware resource
-    /// </summary>
-    Control,
-    /// <summary>
-    /// Full access to the hardware resource (including configuration)
-    /// </summary>
-    FullAccess     
-}
-
-
-public interface IHardware 
-{
-    public abstract int ID {get;set;}
-    /// <summary>
-    /// We will use this as a way to pass some crucial resources to our emulated drivers
-    /// </summary>
-    public abstract Dictionary<object, object> DriverInfo {get;set;}
-    /// <summary>
-    /// We'll use this just as a descrption for the specific piece of hardware, like serial no, manufacturer, name, architecture, whatever it may be.
-    /// </summary>
-    public abstract Dictionary<string, string> HardwareSpecs {get;set;}
-    public abstract int[] Initialize(params object[] parameters);
-    public abstract int[] Finalize(params object[] parameters);
-}
-
-public interface IMemoryDevice : IHardware
-{
-   
-    /// <summary>
-    /// Reads the memory from the specified address.
-    /// </summary>
-    /// <param name="address"></param>
-    /// <returns></returns>
-    public abstract byte[] Read(int address);
-    /// <summary>
-    /// Writes to the device at address with data.
-    /// </summary>
-    /// <param name="address"></param>
-    /// <param name="memory"></param>
-    /// <returns></returns>
-    public abstract bool Write(int address, byte[] memory);
-    /// <summary>
-    /// Returns an action that will be used to fully factory reset the memory device.
-    /// </summary>
-    /// <returns></returns>
-    public abstract Action GetDeviceResetAction();
-}
-
-/// <summary>
-/// Enumerates the processing statuses of a processing device.
-/// </summary>
-public enum ProcessingStatus
-{
-    /// <summary>
-    /// The device is idle or not currently processing any tasks.
-    /// </summary>
-    Idle,
+namespace VM.Hardware{
 
     /// <summary>
-    /// The device is actively processing a task.
+    /// This enum controls the level of access a particular IHardware device emulator grants to the user.
     /// </summary>
-    Active,
-
+    public enum UserPermissions
+    {
+        /// <summary>
+        /// No access to the hardware resource
+        /// </summary>
+        None,          
+        /// <summary>
+        /// Read-only access
+        /// </summary>
+        Read,
+        /// <summary>
+        ///  Write-only access
+        /// </summary>
+        Write,         
+        /// <summary>
+        /// Read and write access
+        /// </summary>
+        ReadWrite,    
+        /// <summary>
+        /// Control or manage the hardware resource
+        /// </summary>
+        Control,
+        /// <summary>
+        /// Full access to the hardware resource (including configuration)
+        /// </summary>
+        FullAccess     
+    }
+    public interface IHardware 
+    {
+        public abstract Dictionary<string, object> PhysicalProperties { get; set;}
+        public abstract int ID {get;set;}
+        /// <summary>
+        /// We will use this as a way to pass some crucial resources to our emulated drivers
+        /// </summary>
+        public abstract Dictionary<object, object> DriverInfo {get;set;}
+        /// <summary>
+        /// We'll use this just as a descrption for the specific piece of hardware, like serial no, manufacturer, name, architecture, whatever it may be.
+        /// </summary>
+        public abstract Dictionary<string, string> HardwareSpecs {get;set;}
+        public abstract int[] Initialize(params object[] parameters);
+        public abstract int[] Finalize(params object[] parameters);
+    }
+    public interface IMemoryDevice : IHardware
+    {
+    
+        /// <summary>
+        /// Reads the memory from the specified address.
+        /// </summary>
+        /// <param name="address"></param>
+        /// <returns></returns>
+        public abstract byte[] Read(int address);
+        /// <summary>
+        /// Writes to the device at address with data.
+        /// </summary>
+        /// <param name="address"></param>
+        /// <param name="memory"></param>
+        /// <returns></returns>
+        public abstract bool Write(int address, byte[] memory);
+        /// <summary>
+        /// Returns an action that will be used to fully factory reset the memory device.
+        /// </summary>
+        /// <returns></returns>
+        public abstract Action GetDeviceResetAction();
+    }
     /// <summary>
-    /// An error occurred during processing, and the device is in an error state.
+    /// Enumerates the processing statuses of a processing device.
     /// </summary>
-    Error,
+    public enum ProcessingStatus
+    {
+        /// <summary>
+        /// The device is idle or not currently processing any tasks.
+        /// </summary>
+        Idle,
 
+        /// <summary>
+        /// The device is actively processing a task.
+        /// </summary>
+        Active,
+
+        /// <summary>
+        /// An error occurred during processing, and the device is in an error state.
+        /// </summary>
+        Error,
+
+        /// <summary>
+        /// The device is paused and not processing tasks.
+        /// </summary>
+        Paused
+    }
     /// <summary>
-    /// The device is paused and not processing tasks.
+    /// Represents an interface for processing devices.
     /// </summary>
-    Paused
-}
+    public interface IProcessingDevice : IHardware
+    {
+        /// <summary>
+        /// Gets the current processing status of the device.
+        /// </summary>
+        ProcessingStatus Status { get; }
 
-public interface IProcessingDevice : IHardware
-{
+        /// <summary>
+        /// Executes a processing task with the given input.
+        /// </summary>
+        /// <param name="input">Input data for the processing task.</param>
+        /// <returns>Output data produced by the processing task.</returns>
+        Task<object> ExecuteProcessingTask(object input);
 
+        /// <summary>
+        /// Pauses the processing device, temporarily halting task execution.
+        /// </summary>
+        void Pause();
 
+        /// <summary>
+        /// Resumes processing after a pause.
+        /// </summary>
+        void Resume();
 
+        /// <summary>
+        /// Resets the processing device, clearing any error states or pending tasks.
+        /// </summary>
+        void Reset();
+    }
 }
