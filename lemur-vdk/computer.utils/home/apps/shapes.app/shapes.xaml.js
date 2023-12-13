cbc2d0a00163936b4fe09f072727612596e797d5
toolateralus <109556963+toolateralus@users.noreply.github.com>
2023-12-13 17:27:36 -0500

    update shape

--- lemur-vdk/computer.utils/home/apps/shapes.app/shapes.xaml.js
+++ lemur-vdk/computer.utils/home/apps/shapes.app/shapes.xaml.js
@@ -1,21 +1,57 @@
 const { Node, Vec2 } = require('game.js');
 class shapes {
     constructor(id) {
+        
         this.gfx_ctx = new GraphicsContext(id, 'RenderingTarget', 256, 256);
         App.eventHandler('this', 'm_Rendering', Event.Rendering);
-        this.player = new Node(new Vec2(25, 25), new Vec2(1, 1));
-        this.player.velocity = new Vec2(25, 25);
-        this.player.drag = 1;
+        
+        const player = new Node(new Vec2(25, 25), new Vec2(256, 256));
+        player.velocity = new Vec2(0, 2500);
+        player.drag = 1;
+        
         this.min = new Vec2(0, 0);
         this.max = new Vec2(256, 256);
+        
+        this.player = player;
+        
         App.defer('navigate', 1);
     }
     
-    navigate () {
-		App.defer('navigate', 2500);
-		this.player.velocity.set(this.player.velocity.y, this.player.velocity.x);
-    }
+   	navigate() {
+
+	    const vel = this.player.velocity;
+	    const dir = Math.floor(Math.random() * 6); // Generate a random direction (0 to 5)
+	
+		
+	
+	    switch (dir) {
+	        case 0:
+	            vel.set(0, 1);
+	            break;
+	        case 1:
+	            vel.set(0, -1);
+	            break;
+	        case 2:
+	            vel.set(1, 0);
+	            break;
+	        case 3:
+	            vel.set(-1, 0);
+	            break;
+	        case 4:
+	            vel.set(1, 1);
+	            break;
+	        case 5:
+	            vel.set(-1, -1); 
+	            break;
+	    }
     
+    	vel.mult(200);
+    	
+   		notify(dir)
+    	App.defer('navigate', Math.floor(random() * 10000));
+    
+	}
+
     m_Rendering() {
     	this.player.clamp_position(this.min, this.max);
     	this.player.update_physics(16 / 1000);
